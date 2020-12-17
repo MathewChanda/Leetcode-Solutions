92354dffc07ed1100ad8a6ae90626ddccabebe64
MathewChanda <ichanda72@gmail.com>
2020-12-16 21:04:11 -0500

    added comments

--- Easy/217. Contains Duplicate Numbers.java
+++ Easy/217. Contains Duplicate Numbers.java
@@ -2,13 +2,17 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashMap<Integer,Integer> pairs = new HashMap<Integer,Integer>(); 
         
+        // loop through each of the elements in the array 
         for(int index = 0; index < nums.length; index++){ 
+            // grab current element 
             int number = nums[index]; 
             
+            // if element is already in the hashmap, return true 
             if(pairs.containsKey(number) == true){
                 return true; 
             }
             
+            // else put it in the hash map 
             pairs.put(number,1); 
         }
         
